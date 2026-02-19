78306e3f657793711e52ba82e04ced029bad5084
amina123-ass <aminatitoua342@gmail.com>
2026-02-19 13:00:05 +0000

    Fix namespace and remove cookie exceptions

--- backend/app/Http/Middleware/EncryptCookies.php
+++ backend/app/Http/Middleware/EncryptCookies.php
@@ -2,17 +2,11 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\\Cookie\\Middleware\\EncryptCookies as Middleware;
+use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
 
 class EncryptCookies extends Middleware
 {
-    /**
-     * The names of the cookies that should not be encrypted.
-     *
-     * @var array<int, string>
-     */
-    protected \$except = [
+    protected $except = [
         //
     ];
 }
-
